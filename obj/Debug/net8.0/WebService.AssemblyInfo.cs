a143274c122bd26b20e1c9ea616c1bdfe1d45dc5
Anh Dang Tuan <anh.dangtuan@nashtechglobal.com>
2024-06-19 08:08:44 +0000

    Jun 19, 2024, 3:08 PM

--- obj/Debug/net8.0/WebService.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+733334fbd9428b6a28750368e19feb5fcb8b1b11")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
